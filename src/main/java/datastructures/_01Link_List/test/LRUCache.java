1502351ccbb7877f82962f315789c42f69e1198a
李志 <2733827845@qq.com>
2026-07-14 17:10:14 +0800

    feat: LRUCache.java
    LRUCache.java

---
+++ src/main/java/datastructures/_01Link_List/test/LRUCache.java
@@ -0,0 +1,27 @@
+package datastructures._01Link_List.test;
+
+import java.util.LinkedList;
+
+/**
+ * @Author: Lee
+ * @Description: 146. LRU 缓存
+ * @DateTime: 2026/7/14 下午4:56
+ **/
+public class LRUCache {
+
+
+
+
+    public LRUCache(int capacity) {
+
+    }
+
+    public int get(int key) {
+
+        return 0;
+    }
+
+    public void put(int key, int value) {
+
+    }
+}
